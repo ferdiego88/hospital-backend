b9287c12ac78fdf7bd7b9ea0ad482c4d6ccb0e10
Fernando Santillan <ferdiego88@gmail.com>
2021-03-25 19:08:39 -0500

    Fin de la Seccion 10 - Auth

--- index.js
+++ index.js
@@ -11,19 +11,20 @@ const app = new express();
 // Configurar CORS
 app.use(cors());
 
+// Lectura y Parseo del Body
+app.use(express.json());
+
 // Base de Datos
 dbConnection();
+
 // console.log(process.env);
 // Usuario Mongo: mean_user
 // password Mongo: yPO7b5u7TEmJwVvn
 
 // Rutas
-app.get('/', (req, res) => {
-    res.json({
-        ok: true,
-        msg: 'Hola Mundo'
-    })
-});
+app.use('/api/usuarios', require('./routes/usuarios'));
+app.use('/api/login', require('./routes/auth'));
+
 
 app.listen(process.env.PORT, () => {
     console.log('Servidor corriendo en el Puerto', process.env.PORT);
---
+++ routes/usuarios.js
@@ -0,0 +1,38 @@
+/* 
+    Ruta : /api/usuarios
+*/
+const { Router } = require('express');
+const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
+const { validarJWT } = require('../middlewares/validar-jwt');
+const { getUsuarios, crearUsuario, actualizarUsuario, eliminarUsuario } = require('../controllers/usuarios');
+
+const router = Router();
+
+router.get('/', validarJWT, getUsuarios);
+
+router.post('/', [check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+        check('email', 'El email es obligatorio').not().isEmpty(),
+        check('password', 'El password es obligatorio').not().isEmpty(),
+        validarCampos
+    ],
+    crearUsuario
+);
+
+router.put('/:id', [
+        validarJWT,
+        check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+        check('email', 'El email es obligatorio').isEmail(),
+        check('role', 'El role es obligatorio').not().isEmpty(),
+        validarCampos
+    ],
+    actualizarUsuario
+);
+
+router.delete('/:id',
+    validarJWT,
+    eliminarUsuario
+);
+
+
+module.exports = router;
